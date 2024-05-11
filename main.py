3da4121c0c8c66d4011270b17c74dc16a558c334
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2024-05-11 16:28:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def send_message():
     <head>
         <style>
         body {
-        background-image: url('https://picjj.com/images/2024/05/11/NBtPv.mp4');
+        background-image: url('https://picjj.com/images/2024/05/11/NTpf2.jpg');
         background-size: cover;
     }
     body {
